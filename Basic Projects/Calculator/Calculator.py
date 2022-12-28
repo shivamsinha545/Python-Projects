934816c11e0f684ba375935a6f897d8cf9471ae9
shiva <shivamsinha545@gmail.com>
2022-12-28 15:54:23 +0530

    total update

--- Basic Projects/Calculator/Calculator.py
+++ Basic Projects/Calculator/Calculator.py
@@ -1,21 +1,49 @@
-# miniproject
-#calculator
+def add(numbers):
+    sum = 0
+    for i in numbers:
+        sum += i
+    return sum
 
-num1=int(input("enter your first number: "))
-operator=input("Enter oepration (+,-,/,x): ")
-num2=int(input("Enter your second number: "))
 
-if(operator=="/"):
-    if(num1>num2):
-        print(num1/num2)
-    else:
-        print(num2/num1)
-elif(operator=='-'):
-    if(num1>num2):
-        print(num1-num2)
-    else:
-        print(num2-num1)
-elif(operator=='*'):
-    print(num1*num2)
+def sub(numbers):
+    dif = 0
+    for i in numbers:
+        dif -= i
+    return dif
+
+
+def divide(numbers):
+    numbers = sorted(numbers, reverse=True)
+    div = numbers[0]
+    i = numbers[1]
+    for i in numbers:
+        div /= i
+    return div
+
+
+def multiply(numbers):
+    product = 1
+    for i in numbers:
+        product *= i
+    return product
+
+
+operations = input("Enter the operation you want to perform: +, -, *, / :")
+numbers = []
+print("Enter the Operands, if finished(enter done)")
+while True:
+    operand = input("->")
+    if operand == 'done' or operand == 'Done' or operand == 'DONE':
+        break
+    numbers.append(int(operand))
+
+if operations == '+':
+    print("The sum is:", add(numbers))
+elif operations == "-":
+    print("The difference is: ", sub(numbers))
+elif operations == '*':
+    print("The product is: ", multiply(numbers))
+elif operations == '/':
+    print("After division the answer is: ", divide(numbers))
 else:
-    print(num1+num2)
+    print("INVALID OPERATIONS!! TRY AGAIN")
